79b8c6f26e16ce977db597060074539f5f402fad
Dave Nuñez <dnunez24@gmail.com>
2017-02-10 00:11:43 -0800

    Add basis for PHP code sniffing

---
+++ scripts/composer/ScriptHandler.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Dnunez24\composer;
+
+use Composer\Script\Event;
+use Composer\Installer\PackageEvent;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\Filesystem\Io\File;
+
+class ScriptHandler
+{
+    public static function postCreateProject()
+    {
+
+    }
+}
